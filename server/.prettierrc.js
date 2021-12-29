f24f5560a3c0a9bb823f4372e590077befcaed1c
nahee-park <jessica396@ewhain.net>
2021-12-30 03:01:04 +0900

    dev: 린트, 프리티어 설정

---
+++ server/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 200,
+  tabWidth: 2,
+};
